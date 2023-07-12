47bcd52345e0dc112b1c5967c669de73bf14f28e
Rence_Madali <jan.lawrence.madali@gmail.com>
2023-07-12 17:28:10 +0800

    update cors origin url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { errorHandler } = require('./middleware')
 const app = express()
 // middleware
 app.use(bodyParser.json())
-app.use(cors({ origin:'http://localhost:4200'}))
+app.use(cors({ origin:'https://eternal-plans.netlify.app'}))
 app.use('/api/eternalplans', eternalplansRoutes)
 app.use(errorHandler)
 
